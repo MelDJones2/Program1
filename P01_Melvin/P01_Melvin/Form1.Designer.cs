5f657cfc821cc8c3b9655bcce566c44efff237db
Mel Jones <121836871+MelJ03@users.noreply.github.com>
2024-04-22 16:14:10 -0500

    Add files via upload
    Fingers crossed that this uploads correctly. ())_())

---
+++ P01_Melvin/P01_Melvin/Form1.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace P01_Melvin
+{
+    partial class program1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnTalk = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.lblMessage = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnTalk
+            // 
+            this.btnTalk.Location = new System.Drawing.Point(151, 250);
+            this.btnTalk.Name = "btnTalk";
+            this.btnTalk.Size = new System.Drawing.Size(250, 64);
+            this.btnTalk.TabIndex = 0;
+            this.btnTalk.Text = "Talk";
+            this.btnTalk.UseVisualStyleBackColor = true;
+            this.btnTalk.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(447, 251);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(250, 63);
+            this.btnExit.TabIndex = 1;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // lblMessage
+            // 
+            this.lblMessage.AutoSize = true;
+            this.lblMessage.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMessage.Location = new System.Drawing.Point(395, 74);
+            this.lblMessage.Name = "lblMessage";
+            this.lblMessage.Size = new System.Drawing.Size(56, 25);
+            this.lblMessage.TabIndex = 2;
+            this.lblMessage.Text = "Hello";
+            this.lblMessage.Click += new System.EventHandler(this.label1_Click_1);
+            // 
+            // program1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(816, 468);
+            this.Controls.Add(this.lblMessage);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnTalk);
+            this.Name = "program1";
+            this.Text = "Program 1";
+            this.Load += new System.EventHandler(this.program1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnTalk;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Label lblMessage;
+    }
+}
+
